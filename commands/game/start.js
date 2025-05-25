21f2ce5746696102d2f0fc69ff11c661ccf70ffc
The Lazy Guy <103317378+imthelazyguy@users.noreply.github.com>
2025-05-25 20:56:53 +0600

    Update start.js

--- commands/game/start.js
+++ commands/game/start.js
@@ -1,55 +1,22 @@
-// commands/game/start.js
-
 const { SlashCommandBuilder, ChannelType, PermissionFlagsBits } = require('discord.js');
-const { sendAdminControlPanel } = require('../../components/admin/controlPanel');
-const { initializeGame } = require('../../components/game/gameInitializer');
 const { setGameConfig } = require('../../utils/dbUtils');
+const { sendAdminControlPanel } = require('../../commands/admin/controlPanel');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('start')
-    .setDescription('Start a new Werewolf game in this server')
-    .addChannelOption(option =>
-      option.setName('voice_channel')
-        .setDescription('Voice channel where players will join')
-        .setRequired(true)
-        .addChannelTypes(ChannelType.GuildVoice)
-    ),
-
+    .setDescription('Start a new game')
+    .addChannelOption(opt => opt.setName('voice_channel').setDescription('VC for game').setRequired(true).addChannelTypes(ChannelType.GuildVoice)),
   async execute(interaction) {
-    try {
-      const voiceChannel = interaction.options.getChannel('voice_channel');
-
-      // Permission check
-      if (!interaction.member.permissions.has(PermissionFlagsBits.ManageGuild)) {
-        return await interaction.reply({ content: 'You do not have permission to start a game.', ephemeral: true });
-      }
-
-      await interaction.deferReply({ ephemeral: true });
-
-      // Set up game in database
-      const hostId = interaction.user.id;
-      const guildId = interaction.guild.id;
-
-      await setGameConfig(guildId, {
-        host: hostId,
-        voiceChannelId: voiceChannel.id,
-        phase: 'waiting',
-        players: [],
-        roles: [],
-        votes: {},
-        gameActive: true,
-      });
-
-      await initializeGame(interaction.guild, voiceChannel, hostId);
-
-      // Send control panel
-      await sendAdminControlPanel(interaction.channel, hostId);
-
-      await interaction.editReply('Game setup is complete. Admin control panel sent.');
-    } catch (error) {
-      console.error('Error in /start command:', error);
-      await interaction.editReply('An error occurred while starting the game.');
+    if (!interaction.member.permissions.has(PermissionFlagsBits.ManageGuild)) {
+      return interaction.reply({ content: 'No permission.', ephemeral: true });
     }
+    await interaction.deferReply({ ephemeral: true });
+    const vc = interaction.options.getChannel('voice_channel');
+    const host = interaction.user.id;
+    const guildId = interaction.guildId;
+    await setGameConfig(guildId, { host, voiceChannelId: vc.id, phase: 'waiting', players: [], gameActive: true });
+    await sendAdminControlPanel(interaction.channel, host);
+    await interaction.editReply('Game started and control panel sent.');
   }
 };
