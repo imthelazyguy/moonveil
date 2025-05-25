89feaa0dacc6aca1366f996ed81cef310576dae6
The Lazy Guy <103317378+imthelazyguy@users.noreply.github.com>
2025-05-25 20:57:53 +0600

    Update setup.js

--- commands/admin/setup.js
+++ commands/admin/setup.js
@@ -1,34 +1,16 @@
-// commands/admin/setup.js
-
 const { SlashCommandBuilder, ChannelType, PermissionFlagsBits } = require('discord.js');
 const { setGuildConfig } = require('../../utils/dbUtils');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('setup')
-    .setDescription('Set up Moonveil bot configuration')
-    .addChannelOption(option =>
-      option.setName('admin_channel')
-        .setDescription('Text channel where admin control panel will be sent')
-        .setRequired(true)
-        .addChannelTypes(ChannelType.GuildText)
-    ),
-
+    .setDescription('Configure Moonveil admin channel')
+    .addChannelOption(opt => opt.setName('admin_channel').setDescription('Text channel for admin panel').setRequired(true).addChannelTypes(ChannelType.GuildText)),
   async execute(interaction) {
-    try {
-      if (!interaction.member.permissions.has(PermissionFlagsBits.Administrator)) {
-        return await interaction.reply({ content: 'You need Administrator permission to run this command.', ephemeral: true });
-      }
-
-      const adminChannel = interaction.options.getChannel('admin_channel');
-      const guildId = interaction.guild.id;
-
-      await setGuildConfig(guildId, { adminChannelId: adminChannel.id });
-
-      return await interaction.reply(`Admin channel has been set to ${adminChannel}.`);
-    } catch (error) {
-      console.error('Error in /setup command:', error);
-      return await interaction.reply({ content: 'There was an error during setup.', ephemeral: true });
-    }
+    if (!interaction.member.permissions.has(PermissionFlagsBits.Administrator))
+      return interaction.reply({ content: 'Admin only.', ephemeral: true });
+    const ch = interaction.options.getChannel('admin_channel');
+    await setGuildConfig(interaction.guildId, { adminChannelId: ch.id });
+    await interaction.reply(`Admin channel set to ${ch}.`);
   }
 };
